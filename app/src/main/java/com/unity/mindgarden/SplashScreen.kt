5fd5447a1429e41e76f32de75ebe52ad9f08e422
atzulf <atzulfikar45@gmail.com>
2025-05-10 23:05:47 +0700

    feat: SplashScreen

---
+++ app/src/main/java/com/unity/mindgarden/SplashScreen.kt
@@ -0,0 +1,27 @@
+package com.unity.mindgarden
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        fun goToOnboarding1() {
+            Intent(this, onboarding1::class.java).also {
+                startActivity(it)
+                finish()
+            }
+        }
+            super.onCreate(savedInstanceState)
+            setContentView(R.layout.activity_splash_screen)
+
+            Handler(Looper.getMainLooper()).postDelayed({
+                goToOnboarding1()
+            }, 3000L)
+    }
+}
